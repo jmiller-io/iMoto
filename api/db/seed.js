dbfca0b4280165e1a081ad31dfbad2df4bf38e8f
Jake Miller <hillsighed@gmail.com>
2017-03-05 11:59:44 -0800

    seeds more data

--- api/db/seed.js
+++ api/db/seed.js
@@ -8,9 +8,22 @@ var mcycle = new Motorcycle({
   make: 'Yamaha',
   model: 'FZ6'
 })
-
 mcycle.save();
 
+var mcycle2 = new Motorcycle({
+  year: 2017,
+  make: 'Honda',
+  model: 'CRF250L Rally'
+})
+mcycle2.save();
+
+
+var mcycle3 = new Motorcycle({
+  year: 2016,
+  make: 'Yamaha',
+  model: 'FZ09'
+})
+mcycle3.save();
 // var me = new User({name: 'Jake'})
 // me.motorcycles.push(mcycle);
 // me.save();
