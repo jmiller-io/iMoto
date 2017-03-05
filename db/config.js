b85ffd93c3c63ce5c6f42644f4f159ec121fe518
Jake Miller <hillsighed@gmail.com>
2017-03-05 10:13:28 -0800

    removes hbs views and creates index.html

--- db/config.js
+++ db/config.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 mongoose.Promise = Promise
-const url = process.env.MONGODB_URI || 'mongodb://localhost/sandbox'
+const url = process.env.MONGODB_URI || 'mongodb://localhost/imoto-app'
 
 mongoose.connect(url)
 mongoose.connection.once('open', function () {
