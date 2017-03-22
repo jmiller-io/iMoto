8801387846f33c7521afdfd0f2a68c15c39d500b
Jake Miller <hillsighed@gmail.com>
2017-03-22 10:54:26 -0700

    cleaning up uneeded commented code

--- frontend/js/controllers/motorcyclesDetailController.js
+++ frontend/js/controllers/motorcyclesDetailController.js
@@ -23,7 +23,6 @@ angular.module('iMotoApp')
     }
 
     function editMotoDetails() {
-      console.log('hi from funct')
       $scope.showMotoInfo = false;
       $scope.showMotoEditForm = true;
     }
@@ -36,29 +35,7 @@ angular.module('iMotoApp')
           $state.go('home', null, { reload: true });
           MotorcyclesService.getMotorcycles()
         })
-      // $http
-      //   .post('http://127.0.0.1:3000/motorcycles', vm.newMotorcycle)
-      //   .then(function(response) {
-      //     $state.go('home', null, { reload: true });
-      //     MotorcyclesService.getMotorcycles()
-      //   })
-      // vm.newMotorcycle = {};
     }
-
-
-
-    // function editMoto(motorcycle) {
-    //   var $form = $('<form ng-submit="motorcycle.saveEdits()">');
-    //   var $forminputmotoyear = $(`<label for="year">Year:</label><input type="number" name="year" value="${motorcycle.year}" ng-model="motorcycle.editMotorcycle.year">`)
-    //   $forminputmotoyear.appendTo($form)
-    //   var $forminputmotomake = $(`<label for="make">Make:</label><input type="text" name="make" value="${motorcycle.make}" ng-model="motorcycle.editMotorcycle.make">`)
-    //   $forminputmotomake.appendTo($form)
-    //   var $forminputmotomodel = $(`<label for="model">Model:</label><input type="text" name="model" value="${motorcycle.model}" ng-model="motorcycle.editMotorcycle.model">`)
-    //   $forminputmotomodel.appendTo($form)
-    //   var $formSave = $('<input class="btn" type="submit" value="Save">')
-    //   $formSave.appendTo($form)
-    //   $('#motoInfoTable').replaceWith($form)
-    // }
 
     $scope.selectedMotorcycle = MotorcyclesService.findMotorcycles($stateParams.id)
   }
