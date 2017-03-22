0bc0519e00307d3dd112487cde4482e252c66eff
Jake Miller <hillsighed@gmail.com>
2017-03-22 10:52:42 -0700

    misc styling changes

--- frontend/js/controllers/motorcyclesDetailPartController.js
+++
@@ -1,7 +0,0 @@
-angular.module('iMotoApp')
-  .controller('MotorcyclesDetailPartController', MotorcyclesDetailPartController)
-
-  MotorcyclesDetailPartController.$inject = ['$scope', '$stateParams', 'MotorcyclesService'];
-  function MotorcyclesDetailPartController($scope, $stateParams, MotorcyclesService) {
-    $scope.selectedMotorcycle = MotorcyclesService.find($stateParams.id)
-  }
